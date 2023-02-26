b88e1e58841ebabdd7c36846a3d98ef31f831561
LizaPasina <pasinaliza8@gmail.com>
2023-02-26 13:45:18 +0300

    initial commit

--- PZ_13/PZ_13_1.py
+++ PZ_13/PZ_13_1.py
@@ -7,8 +7,14 @@ while ch < 5:
     a = [random.randint(-15,15) for n in range(1,7)]
     b.append(a)
     ch += 1
-print(b)
+b1 = []
 for i in b:
-  if b[i] % 3 == 0:
-    b[i] = b[i] * 3
-print(b)
+    a1 = []
+    for f in i:
+        if f % 3 == 0:
+            a1.append(f * 3)
+        else:
+            a1.append(f)
+    b1.append(a1)
+print('Изначальная матрица: ', b)
+print('Матрица с увеличенными элементами: ', b1)
