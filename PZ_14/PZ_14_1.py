db45d35eaa57c781f400f3f0d576aa162729b7ae
LizaPasina <pasinaliza8@gmail.com>
2023-03-07 19:43:17 +0300

    otpr

--- PZ_14/PZ_14_1.py
+++ PZ_14/PZ_14_1.py
@@ -1,7 +1,13 @@
 # Из исходного текстового файла (expansion.txt) выбрать имена фалов,
-# соответствующие типам: .xls, .xml, .html, .css, .py. Посчитать количество полученных
+# соответствующие типам: .xls, .xml, .html, .css, .py.Посчитать количество полученных
 # элементов.
+import re
 
-f1 = open('expansion.txt')
-k = f1.read()
-
+f = open('expansion.txt', encoding="utf8")
+count = 0
+for i in f:
+    p = re.findall(r'\.xls|.xml|.html|.css|.py', i)
+    if len(p) > 0:
+        count += 1
+        print(i)
+print('Количество элементов:', count)
